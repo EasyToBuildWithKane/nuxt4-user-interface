db65388b941329a0143294380b2b7ab56308fc08
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-08-25 13:06:08 +0700

    Testing Setup

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
+
+<script setup>
+
+useHead({
+  htmlAttrs: { lang: "en" },
+  title: "Kane - Personal Portfolio",
+  meta: [
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+    { charset: "utf-8" },
+    {
+      name: "description",
+      content:
+        "Portfolio of Kane - Fullstack Developer, expert in Nuxt 4 and Laravel.",
+    },
+    { property: "og:title", content: "Kane - Personal Portfolio" },
+    { property: "og:type", content: "website" },
+  ],
+  link: [
+    { rel: "icon", type: "image/png", href: "/images/favicon.png" },
+    { rel: "shortcut icon", type: "image/png", href: "/images/favicon.png" },
+  ],
+});
+</script>
+
